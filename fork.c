01ab53f3532c6aa5a2535208179852296b49516f
Javier Sanchez Panduro <javiersanpan@outlook.com>
2021-10-19 18:17:54 +0000

    minamay

--- fork.c
+++ fork.c
@@ -3,11 +3,6 @@
 
 int main(){
 
-    // este bucle infiníto crea procesos infinitos, y como se llenan los ID
-    // ya no se pueden usar los demás programas
-    // while(true){
-
-
     int pid = fork();
     /* A partir de esta línea, van a haber dos procesos que van a ser idénticos
     van a tener el mismo código, excepto que pid en el hijo va a ser 0, y en el padre 
@@ -18,7 +13,6 @@ int main(){
     } else {
         printf("Soy el proceso padre = pid: %d\n",pid);
     }
-    //}
     
     return 0;
 }
